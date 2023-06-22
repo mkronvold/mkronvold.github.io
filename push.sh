f3ad9898a3f43ab12a392bf389a449676c717462
mkronvold <>
2023-06-22 14:33:56 -0500

    202306221433

--- push.sh
+++ push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##### Set homedir to point to where you edit your org files (optional)
+HOMEDIR=/mnt/c/Users/mike/OneDrive/org
+
 cleanup () {
      #  Delete temporary files, then optionally exit given status.
      local status=${1:-'0'}
@@ -21,8 +24,31 @@ trap "die 'SIG disruption, but cleanup finished.' 114" 1 2 3 15
 
 hr () { printf "%0$(tput cols)d" | tr 0 ${1:-=}; }
 
+
+##### Batch processing doesn't prompt for sync or push
+[ "${1}" == "-b" ] && BATCH=1 || BATCH=
+
+
+confirmsync () {
+ [ "${BATCH}" ] && REPLY = n || REPLY=0
+ while true
+ do
+   hr ; echo -e "\n"
+   echo -e "\t\tPress 'y' to sync home to local\t\tPress 'n' to use local .org files as is\t\tPress 'x' to quit"
+   hr
+   read -n1 -s
+   case "$REPLY" in
+     x | X ) die "======> Exiting leaving everything as is" ;;
+     n | N ) echo "======> Using local .org files" ; break ;;
+     y | Y ) for i in $(cat ./orglist); do cp -v ${HOMEDIR}/${i}.org ./${i}.org ; done ; echo "======> Copied from ${HOMEDIR}" ; break ;;
+         * ) echo "" ;;
+   esac
+ done
+}
+
+
 confirmpush () {
- REPLY=h
+ [ "${BATCH}" ] && REPLY = y || REPLY=0
  while true
  do
    hr ; echo -e "\n"
@@ -38,15 +64,29 @@ confirmpush () {
  done
 }
 
+##### Check to see if a commit message was specified on command line otherwise use the date
+[ "${1}" == "-m" ] && MSG="${2}" || MSG="$(date +%Y%m%d%H%M)"
 
-[ "${1}" ] && MSG="${1}" || MSG="$(date +%Y%m%d%H%M)"
-
+##### Find differences between files in homedir and local repo
+DIFFS=
 if [ -f ./orglist ]; then
-  for i in $(cat ./orglist); do ./render-emacs-org-to-html.sh $i; done
+  for i in $(cat ./orglist); do
+    diff -q ${i}.org ${HOMEDIR}/${i}.org
+    DIFFS=1
+  done
+fi
+[ "${DIFFS}" ] && confirmsync
+
+##### render all the org files in the orglist
+if [ -f ./orglist ]; then
+  for i in $(cat ./orglist); do
+    ./render-emacs-org-to-html.sh $i
+  done
 else
   ./render-emacs-org-to-html.sh
 fi
 
+##### add, commit and show the changes
 git add -A 1> /dev/null
 git commit -m "$MSG" 1> /dev/null
 hr
@@ -54,6 +94,7 @@ echo -e "\r\t\tThese changes will be pushed\n"
 git push --dry-run
 git diff --stat --cached origin/main | cat
 
+##### then confirm the push and show the results
 confirmpush
 
 git status
